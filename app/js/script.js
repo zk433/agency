6085eb9a907aa558c3941aa9676a69580634493a
Zuzana K <zk0433@gmail.com>
2017-08-11 23:35:22 +0100

    scroll up button added, modals functionality completed, basic interactivity completed

--- app/js/script.js
+++ app/js/script.js
@@ -55,32 +55,86 @@ window.onclick = function(event) {
 }
 
 //******Service Modals********
-/*
+// WebDev modal
 // Get the modal
-var modal = document.getElementById('webDev');
+var modalwebdev = document.getElementById('webDev');
 
 // Get the button that opens the modal
-var btn = document.getElementById("webdev_learnMore");
+var btnwebdev = document.getElementById("webdev_learnMore");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[1];
 
 // When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+btnwebdev.onclick = function() {
+    modalwebdev.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+    modalwebdev.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}*/
+// window.onclick = function(event) {
+//     if (event.target == modalwebdev) {
+//         modalwebdev.style.display = "none";
+//     }
+// }
+
+// dev modal
+// Get the modal
+var modaldev = document.getElementById('dev');
+
+// Get the button that opens the modal
+var btndev = document.getElementById("dev_learnMore");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[2];
+
+// When the user clicks on the button, open the modal 
+btndev.onclick = function() {
+    modaldev.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modaldev.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modaldev) {
+//         modaldev.style.display = "none";
+//     }
+// }
+
+// seo modal
+// Get the modal
+var modalseo = document.getElementById('seo');
+
+// Get the button that opens the modal
+var btnseo = document.getElementById("seo_learnMore");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[3];
+
+// When the user clicks on the button, open the modal 
+btnseo.onclick = function() {
+    modalseo.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modalseo.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modalseo) {
+//         modalseo.style.display = "none";
+//     }
+// }
 
 // ******************Smooth scrolling**********************
 $(document).ready(function(){
